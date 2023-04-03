670946244be9aa8fbd5c8a4027adb0b255e07125
Neko-Nika <veronicachastova13@gmail.com>
2023-04-03 12:37:58 +0300

    Add quickstart page

--- app/urls.py
+++ app/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="default"),
+    path("quickstart", views.quickstart, name="default"),
     
 ]
--- app/views.py
+++ app/views.py
@@ -1,6 +1,12 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
 
+@login_required(login_url="/quickstart")
 def index(request):
-    return render(request, "base.html")
+    return HttpResponse("HEllo")
+
+def quickstart(request):
+    return render(request, "quickstart.html")
+
